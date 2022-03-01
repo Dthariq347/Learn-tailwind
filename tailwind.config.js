f2675b034a48c6cd552536ebbcf2670eef6ffaef
Dthariq347 <65454556+Dthariq347@users.noreply.github.com>
2022-03-01 21:44:04 +0700

    bagian 4
    basic utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+        '15': '3.75rem',
+        '128': '32rem',
+        '144': '36rem',
+      }
+    }
   },
   plugins: [],
 }
